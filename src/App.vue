8b8935149131ab10691fde80a162e34cd1b10a1c
xdser <349867227@qq.com>
2018-05-15 16:48:48 +0800

    delete aha

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
     <!-- <HelloWorld/> -->
     <!-- <router-link to="goods" @click="goods"  href="javascript:void(0)">goods</router-link> -->
     <p @click="goods">1111</p>
-    <p>aha</p>
     <transition name="fade">
         <router-view />
     </transition>
